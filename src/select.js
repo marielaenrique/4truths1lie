1f3058ce2d26feb396ec0576133bb2de4c14115a
marienrique <mariela.enrique@gmail.com>
2026-02-22 21:39:51 -0300

    Adding modal for results

---
+++ src/select.js
@@ -0,0 +1,48 @@
+const popupModal = document.getElementById('popup-modal')
+const popupText = document.getElementById('popup-text')
+const popupOk = document.getElementById('popup-ok')
+const popupImg = document.getElementById('popup-img')
+
+function showPopup(message, imgSrc = null) {
+  return new Promise(resolve => {
+    popupText.textContent = message
+
+    if (imgSrc) {
+      popupImg.src = imgSrc
+      popupImg.style.display = "block"
+    } else {
+      popupImg.style.display = "none"
+    }
+
+    popupModal.classList.add('show')
+
+    popupOk.onclick = () => {
+      popupModal.classList.remove('show')
+      resolve()
+    }
+  })
+}
+let wrongImagesPool = [];
+
+export async function checkLie(element, isLie) {
+  if (isLie) {
+    element.classList.add('correct');
+  } else {
+    element.classList.add('wrong');
+  }
+
+  await new Promise(r => setTimeout(r, 50));
+
+  if (isLie) {
+    await showPopup("Correct! That was the lie", "./public/correct.webp");
+  } else {
+    if (wrongImagesPool.length === 0) {
+      wrongImagesPool = ["./public/wrong.webp", "./public/wrong1.webp", "./public/wrong2.webp", "./public/wrong3.webp"];
+    }
+    const randomIndex = Math.floor(Math.random() * wrongImagesPool.length);
+    const randomImg = wrongImagesPool.splice(randomIndex, 1)[0];
+    await showPopup("Oops! That was actually true", randomImg);
+  }
+
+  element.classList.remove('correct', 'wrong');
+}
