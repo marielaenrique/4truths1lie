13573bca95ad3ee324f8e6a6f415c10826724766
marienrique <mariela.enrique@gmail.com>
2026-02-22 21:54:35 -0300

    Updating main

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
-import './style.css'
 import { checkLie } from './select.js'
 
 document.querySelectorAll('.statement').forEach(element => {
+  // Esta línea detecta automáticamente cuál es la mentira por el texto
   const isLie = element.textContent.includes("inst")
+  
   element.onclick = () => checkLie(element, isLie)
 })
