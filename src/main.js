462bb64f102697fee6ffa1f10e7ee9d284631f2c
marielaenrique <93106042+marielaenrique@users.noreply.github.com>
2026-03-02 14:52:14 -0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,6 +2,6 @@ import { checkLie } from './select.js'
 
 document.querySelectorAll('.statement').forEach(element => {
 
-  const isLie = element.textContent.includes("fluently")
+  const isLie = element.textContent.includes("born")
   element.onclick = () => checkLie(element, isLie)
 })
